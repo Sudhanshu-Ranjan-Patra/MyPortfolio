5baca2ff8ad7bbe5bc4aca5cd041e93c7f5b2e00
Sudhanshu Ranjan Patra <patrasudhanshuranjan@gmail.com>
2025-08-01 23:41:01 +0530

    Create Contact.jsx

---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div className="bg-black [font-family:Oswald,sans-serif] font-bold pb-10">
+        
+    </div>
+  )
+}
+
+export default Contact
