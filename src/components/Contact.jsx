c98d1cafc6eb2518939f50216df86874f7685922
Sudhanshu Ranjan Patra <patrasudhanshuranjan@gmail.com>
2025-08-02 23:57:23 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 function Contact() {
   return (
     <div className="bg-black [font-family:Oswald,sans-serif] font-bold pb-10">
-        
+        <div className="contact grid grid-cols-1 sm:grid-cols-2">
+          <div className="left ">
+
+          </div>
+          <div className="right ">
+
+          </div>
+        </div>
     </div>
   )
 }
