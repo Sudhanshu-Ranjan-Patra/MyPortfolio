1cdbc573565733a681150eb90461730a254531b8
Sudhanshu Ranjan Patra <patrasudhanshuranjan@gmail.com>
2025-08-03 23:27:14 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,12 @@ function Contact() {
     <div className="bg-black [font-family:Oswald,sans-serif] font-bold pb-10">
         <div className="contact grid grid-cols-1 sm:grid-cols-2">
           <div className="left ">
-
+            <div className="">
+              <h3 className="text-2xl font-bold text-white mt-5">GET IN TOUCH</h3>
+            </div>
+            <div className="">
+              <h3 className="text-2xl font-bold text-white mt-5">QUICK LINKS</h3>
+            </div>
           </div>
           <div className="right ">
 
