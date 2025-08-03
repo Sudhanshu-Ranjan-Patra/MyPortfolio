1f2ec2b061f7dda2f2a595996e2c8ed7ddc3b731
Sudhanshu Ranjan Patra <patrasudhanshuranjan@gmail.com>
2025-08-03 23:50:32 +0530

    contact updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './components/Home'
 import About from './components/About'
 import Projects from './components/Projects'
 import Skill from './components/Skill'
+import Contact from './components/Contact'
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <About/>
       <Projects/>
       <Skill/>
+      <Contact/>
     </>
   )
 }
