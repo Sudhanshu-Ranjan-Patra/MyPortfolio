b67b6c2b6f807a5a2db0a957efb43760a2f6bc1e
Sudhanshu Ranjan Patra <patrasudhanshuranjan@gmail.com>
2025-09-29 23:34:23 +0530

    responsive project card

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,143 +1,79 @@
 import React from "react";
 
 function Projects() {
+  const projectData = [
+    {
+      title: "Medifit",
+      desc: "Frontend design of a medical shop with product view, search option, and interactive cart section.",
+      tech: "React + Tailwindcss",
+      logo: "../images/medifit-logo.png",
+    },
+    {
+      title: "FoodyZone",
+      desc: "Interactive food ordering app with smart search, category filtering, and smooth navigation.",
+      tech: "React JS + Rest Api",
+      logo: "../images/foodyzone-logo.png",
+    },
+    {
+      title: "TODO application",
+      desc: "Manageing tasks efficiently with a clean UI, deadline tracking, and completion marking.",
+      tech: "Node js + React Js",
+      logo: "../images/todoapp-logo.png",
+    },
+    {
+      title: "Weather App",
+      desc: "Weather forecast web app with real-time API integration for city based weather updates.",
+      tech: "HTML + CSS + JS",
+      logo: "../images/weatherapp-logo.png",
+    },
+    {
+      title: "QR Generator",
+      desc: "A QR Code Generator that converts text or URLs into QR codes using a QR code API",
+      tech: "Html + CSS + JS",
+      logo: "../images/qr-generator-logo.png",
+    },
+    {
+      title: "Web-Bocket Clone",
+      desc: "Static IT services website presenting it's offerings with keyframe animations.",
+      tech: "Html + CSS + JS",
+      logo: "../images/website-clone-logo.png",
+    },
+  ];
+
   return (
     <div className="bg-black [font-family:Oswald,sans-serif] font-bold pb-10">
       <div className="flex flex-col justify-center items-center">
         <button className="button-primary">Projects</button>
         <h2 className="text-4xl font-bold text-white mt-5">WHAT I BUILD</h2>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 mt-15">
-          <div className="flex flex-col bg-[#191C24] w-100 h-65 group relative">
-            <div className="mt-10 ml-20 mr-15">
-              <h2 className="text-3xl uppercase font-bold text-white ">
-                Medifit
-              </h2>
-              <p className="text-[#6C7293]  font-Franklin font-extralight mt-4 leading-relaxed">
-                Frontend design of a medical shop with product view, search
-                option, and interactive cart section.
-              </p>
-              <p className="text-red-600 uppercase font-Franklin font-light mt-5">
-                React + Tailwindcss
-              </p>
-            </div>
-            <div className=" hidden  group-hover:flex transition justify-center items-center ml-87 w-13 h-13 bg-black">
-              <p className="rotate-45 cursor-pointer">❌</p>
-            </div>
+        {/* Responsive Grid */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 mt-15 w-full px-4 sm:px-10 lg:px-25 cursor-pointer">
+          {projectData.map((project, index) => (
+            <div
+              key={index}
+              className="flex flex-col bg-[#191C24] w-full sm:h-[260px] md:h-[2800px] lg:h-[280px] group relative"
+            >
+              <div className="mt-10 mb-10 ml-10 sm:ml-10 md:ml-20 mr-5 sm:mr-10 md:mr-15">
+                <h2 className="text-2xl pl-5 sm:pl-0 sm:text-3xl md:text-3xl uppercase font-bold text-white">
+                  {project.title}
+                </h2>
+                <p className="text-[#6C7293] font-Franklin font-extralight mt-4 leading-relaxed text-sm sm:text-base md:text-base">
+                  {project.desc}
+                </p>
+                <p className="text-red-600 uppercase font-Franklin font-light mt-5 text-sm sm:text-base">
+                  {project.tech}
+                </p>
+              </div>
 
-            <div className="absolute flex justify-center items-center top-9 w-15 h-15 bg-black">
-              <img className="p-3" src="../images/medifit-logo.png"></img>
-            </div>
-          </div>
+              <div className="hidden group-hover:flex transition justify-center items-center absolute bottom-0 right-0 w-12 h-12 bg-black">
+                <p className="rotate-45 cursor-pointer">❌</p>
+              </div>
 
-          <div className="flex flex-col bg-[#191C24] w-100 h-65 group relative">
-            <div className="mt-10 ml-20 mr-15">
-              <h2 className="text-3xl uppercase font-bold text-white ">
-                FoodyZone
-              </h2>
-              <p className="text-[#6C7293]  font-Franklin font-extralight mt-4 leading-relaxed">
-                Interactive food ordering app with smart search, category
-                filtering, and smooth navigation.
-              </p>
-              <p className="text-red-600 uppercase font-Franklin font-light mt-5">
-                React JS + Rest Api
-              </p>
+              <div className="absolute flex justify-center items-center top-7 left-0 w-15 h-15 bg-black">
+                <img className="p-3" src={project.logo} alt={project.title} />
+              </div>
             </div>
-            <div className=" hidden  group-hover:flex transition justify-center items-center ml-87 w-13 h-13 bg-black">
-              <p className="rotate-45 cursor-pointer">❌</p>
-            </div>
-
-            <div className="absolute flex justify-center items-center top-9 w-15 h-15 bg-black">
-              <img className="p-3" src="../images/foodyzone-logo.png"></img>
-            </div>
-          </div>
-
-          <div className="flex flex-col bg-[#191C24] w-100 h-65 group relative">
-            <div className="mt-10 ml-20 mr-15">
-              <h2 className="text-3xl uppercase font-bold text-white ">
-                TODO application
-              </h2>
-              <p className="text-[#6C7293]  font-Franklin font-extralight mt-4 leading-relaxed">
-                Manageing tasks efficiently with a clean UI, deadline tracking,
-                and completion marking.
-              </p>
-              <p className="text-red-600 uppercase font-Franklin font-light mt-5">
-                Node js + React Js{" "}
-              </p>
-            </div>
-            <div className=" hidden  group-hover:flex transition justify-center items-center ml-87 w-13 h-13 bg-black">
-              <p className="rotate-45 cursor-pointer">❌</p>
-            </div>
-
-            <div className="absolute flex justify-center items-center top-9 w-15 h-15 bg-black">
-              <img className="p-3" src="../images/todoapp-logo.png"></img>
-            </div>
-          </div>
-
-          <div className="flex flex-col bg-[#191C24] w-100 h-65 group relative">
-            <div className="mt-10 ml-20 mr-15">
-              <h2 className="text-3xl uppercase font-bold text-white ">
-                Weather App
-              </h2>
-              <p className="text-[#6C7293]  font-Franklin font-extralight mt-4 leading-relaxed">
-                Weather forecast web app with real-time API integration for city
-                based weather updates.
-              </p>
-              <p className="text-red-600 uppercase font-Franklin font-light mt-5">
-                HTML + CSS + JS
-              </p>
-            </div>
-            <div className=" hidden  group-hover:flex transition justify-center items-center ml-87 w-13 h-13 bg-black">
-              <p className="rotate-45 cursor-pointer">❌</p>
-            </div>
-
-            <div className="absolute flex justify-center items-center top-9 w-15 h-15 bg-black">
-              <img className="p-3" src="../images/weatherapp-logo.png"></img>
-            </div>
-          </div>
-
-          <div className="flex flex-col bg-[#191C24] w-100 h-65 group relative">
-            <div className="mt-10 ml-20 mr-15">
-              <h2 className="text-3xl uppercase font-bold text-white ">
-                QR Generator
-              </h2>
-              <p className="text-[#6C7293]  font-Franklin font-extralight mt-4 leading-relaxed">
-                A QR Code Generator that converts text or URLs into QR codes
-                using a QR code API
-              </p>
-              <p className="text-red-600 uppercase font-Franklin font-light mt-5">
-                Html + CSS + JS
-              </p>
-            </div>
-            <div className=" hidden  group-hover:flex transition justify-center items-center ml-87 w-13 h-13 bg-black">
-              <p className="rotate-45 cursor-pointer">❌</p>
-            </div>
-
-            <div className="absolute flex justify-center items-center top-9 w-15 h-15 bg-black">
-              <img className="p-3" src="../images/qr-generator-logo.png"></img>
-            </div>
-          </div>
-
-          <div className="flex flex-col bg-[#191C24] w-100 h-65 group relative">
-            <div className="mt-10 ml-20 mr-15">
-              <h2 className="text-3xl uppercase font-bold text-white ">
-                Web-Bocket Clone
-              </h2>
-              <p className="text-[#6C7293]  font-Franklin font-extralight mt-4 leading-relaxed">
-                Static IT services website presenting it's offerings with keyframe animations.
-              </p>
-              <p className="text-red-600 uppercase font-Franklin font-light mt-5">
-                Html + CSS + JS
-              </p>
-            </div>
-            <div className=" hidden  group-hover:flex transition justify-center items-center ml-87 w-13 h-13 bg-black">
-              <p className="rotate-45 cursor-pointer">❌</p>
-            </div>
-
-            <div className="absolute flex justify-center items-center top-9 w-15 h-15 bg-black">
-              <img className="p-3" src="../images/website-clone-logo.png"></img>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
